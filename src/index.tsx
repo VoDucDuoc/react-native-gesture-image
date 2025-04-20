853cfd0460154e93eb8edf92d3b646c0ecca7683
Duoc Vo <duocvo2501@gmail.com>
2025-04-21 00:28:21 +0700

    chore: update ts config

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
-import Gallery from './Gallery';
+import type { GalleryRef, GalleryProps, ImageViewerProps } from './Type';
+import type { ComponentType } from 'react';
 
-export { default as ImageViewer } from './ImageViewer';
-export type { GalleryRef, GalleryProps, ImageViewerProps } from './Type';
-export default Gallery;
+export type { GalleryRef, GalleryProps, ImageViewerProps };
+
+import GalleryImpl from './Gallery';
+import ImageViewerImpl from './ImageViewer';
+
+const GalleryComponent: ComponentType<GalleryProps> = GalleryImpl;
+const ImageViewerComponent: ComponentType<ImageViewerProps> = ImageViewerImpl;
+
+export { ImageViewerComponent as ImageViewer };
+export default GalleryComponent;
